6b802191b8c670624c26911b3d954beae4929d6d
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2024-03-01 23:59:01 +0900

    [RAON-22] next-theme를 추가합니다. (#6)

--- apps/web/app/ui/layout.tsx
+++ apps/web/app/ui/layout.tsx
@@ -1,5 +1,3 @@
 export default function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mx-auto flex w-full grow flex-col bg-black">{children}</div>
-  );
+  return <div className="mx-auto flex w-full grow flex-col">{children}</div>;
 }
