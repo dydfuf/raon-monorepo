58ff587655a459d54b18a6cff24eefee7a0fc3bd
dydfuf <88dydfuf@naver.com>
2024-03-02 01:10:23 +0900

    [RAON-18] version 컴포넌트 추가 및 github action을 추가합니다.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,9 @@
+import Version from "@repo/ui/Version";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <Version />
+    </div>
+  );
 }
---
+++ packages/ui/src/version.tsx
@@ -0,0 +1,3 @@
+export default function version() {
+  return <div>version 0.0.0</div>;
+}
