c81c516a6200a62265aa302f67aa9d0c869b8ff3
dydfuf <88dydfuf@naver.com>
2024-03-13 23:02:14 +0900

    📚 docs(app): [RAON-36] docs에 breadcrumb, input-otp 문서를 추가합니다
    Changelog: documentation

--- apps/docs/registry/ui/breadcrumb-link.tsx
+++ apps/docs/registry/ui/breadcrumb-link.tsx
@@ -1,38 +1,34 @@
+import Link from "next/link";
+
+import { BreadcrumbPage } from "@repo/ui/components/breadcrumb";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@repo/ui/components/breadcrumb";
+
 export default function BreadcrumbWithCustomSeparator() {
-  return <div>@TODO</div>;
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <Link href="/">Home</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink>
+            <Link href="/components">Components</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
 }
-
-// import Link from "next/link";
-
-// import { BreadcrumbPage } from "@/registry/default/ui/breadcrumb";
-// import {
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   BreadcrumbLink,
-//   BreadcrumbList,
-//   BreadcrumbSeparator,
-// } from "@repo/ui/components/breadcrumb";
-
-// export default function BreadcrumbWithCustomSeparator() {
-//   return (
-//     <Breadcrumb>
-//       <BreadcrumbList>
-//         <BreadcrumbItem>
-//           <BreadcrumbLink>
-//             <Link href="/">Home</Link>
-//           </BreadcrumbLink>
-//         </BreadcrumbItem>
-//         <BreadcrumbSeparator />
-//         <BreadcrumbItem>
-//           <BreadcrumbLink>
-//             <Link href="/components">Components</Link>
-//           </BreadcrumbLink>
-//         </BreadcrumbItem>
-//         <BreadcrumbSeparator />
-//         <BreadcrumbItem>
-//           <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-//         </BreadcrumbItem>
-//       </BreadcrumbList>
-//     </Breadcrumb>
-//   );
-// }
--- apps/docs/registry/ui/breadcrumb-separator.tsx
+++ apps/docs/registry/ui/breadcrumb-separator.tsx
@@ -1,38 +1,34 @@
-export default function BreadcrumbSeparator() {
-  return <div>@TODO</div>;
+import { SlashIcon } from "@radix-ui/react-icons";
+
+import { BreadcrumbPage } from "@repo/ui/components/breadcrumb";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@repo/ui/components/breadcrumb";
+
+export default function BreadcrumbWithCustomSeparator() {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <SlashIcon />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <SlashIcon />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
 }
-
-// import { SlashIcon } from "@radix-ui/react-icons";
-
-// import { BreadcrumbPage } from "@/registry/default/ui/breadcrumb";
-// import {
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   BreadcrumbLink,
-//   BreadcrumbList,
-//   BreadcrumbSeparator,
-// } from "@repo/ui/components/breadcrumb";
-
-// export default function BreadcrumbWithCustomSeparator() {
-//   return (
-//     <Breadcrumb>
-//       <BreadcrumbList>
-//         <BreadcrumbItem>
-//           <BreadcrumbLink href="/">Home</BreadcrumbLink>
-//         </BreadcrumbItem>
-//         <BreadcrumbSeparator>
-//           <SlashIcon />
-//         </BreadcrumbSeparator>
-//         <BreadcrumbItem>
-//           <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-//         </BreadcrumbItem>
-//         <BreadcrumbSeparator>
-//           <SlashIcon />
-//         </BreadcrumbSeparator>
-//         <BreadcrumbItem>
-//           <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-//         </BreadcrumbItem>
-//       </BreadcrumbList>
-//     </Breadcrumb>
-//   );
-// }
