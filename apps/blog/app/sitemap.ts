1612131ae3da2298afeb8e3176d9f8e9d9780db4
dydfuf <88dydfuf@naver.com>
2024-03-26 02:15:26 +0900

    ✨ feat(app): [RAON-15] 코드들을 모두 가져옵니다
    Changelog: feature

---
+++ apps/blog/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { getPostNameList, getPostByName } from "../utils/post";
+import { parseMarkdownMetadata } from "../utils/parseMarkdownMetadata";
+import { MetadataRoute } from "next";
+
+const URL = "https://raondev.vercel.app";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const postList = getPostNameList().map((postName) => ({
+    name: postName,
+    content: getPostByName(postName ?? ""),
+  }));
+  const postsRoute = postList.map(({ name, content }) => {
+    const { date } = parseMarkdownMetadata(content);
+    return {
+      url: `${URL}/posts/${name}`,
+      lastModified: date,
+    };
+  });
+
+  const routes = ["", "/aboutme"].map((route) => ({
+    url: `${URL}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routes, ...postsRoute];
+}
