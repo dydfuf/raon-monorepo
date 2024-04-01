3b7878aa904343994f8c99436ab47aded4432e72
ChoiYongYeol <41016331+dydfuf@users.noreply.github.com>
2024-04-02 01:03:26 +0900

    ✨ feat(app): [RAON-49] Notion API와 연동합니다 (#32)
    * ✨ feat(app): [RAON-49] Notion API와 연동합니다
    
    Changelog: feature
    
    * ✨ feat(app): [RAON-49] 타입 캐스팅 위치를 변경합니다
    
    Changelog: feature
    
    * ✨ feat(app): [RAON-49] 로직을 간소화 합니다
    
    Changelog: feature

---
+++ apps/coffee/app/.server/notion/service.ts
@@ -0,0 +1,35 @@
+import { iteratePaginatedAPI } from "@notionhq/client";
+import { notion } from "./client";
+import { DEFAULT_FIELD_VALUE } from "./constant";
+import { CoffeeInfo } from "../../types/coffee";
+
+export const getCoffeeInfoList = async () => {
+  const coffeeInfoList: CoffeeInfo[] = [];
+
+  for await (const block of iteratePaginatedAPI(notion.databases.query, {
+    database_id: process.env.NOTION_DATABASE_ID ?? "",
+  })) {
+    if ("properties" in block) {
+      const coffeeInfo = Object.entries(block.properties).map(
+        ([key, value]) => {
+          let fieldValue = DEFAULT_FIELD_VALUE;
+          if (value.type === "title") {
+            fieldValue = value.title[0]?.plain_text;
+          } else if (value.type === "rich_text") {
+            fieldValue = value.rich_text[0]?.plain_text;
+          } else if (value.type === "unique_id") {
+            const { prefix, number } = value.unique_id;
+            fieldValue = `${prefix}-${number}`;
+          } else {
+            fieldValue = JSON.stringify(value);
+          }
+          return { [key]: fieldValue ?? DEFAULT_FIELD_VALUE };
+        }
+      );
+
+      coffeeInfoList.push(Object.assign({}, ...coffeeInfo));
+    }
+  }
+
+  return coffeeInfoList;
+};
