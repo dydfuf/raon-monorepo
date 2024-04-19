6a42db5d4ae894f3c3935228748576d43128fca9
dydfuf <88dydfuf@naver.com>
2024-04-20 01:18:44 +0900

    ✨ feat(app): [raon-53] 원두상세 베일링을 추가합니다
    Changelog: feature

--- apps/coffee/app/.server/notion/service.ts
+++ apps/coffee/app/.server/notion/service.ts
@@ -1,7 +1,7 @@
 import { iteratePaginatedAPI } from "@notionhq/client";
 import { notion } from "./client";
-import { DEFAULT_FIELD_VALUE } from "./constant";
 import { CoffeeInfo, CoffeeInfoField } from "../../types/coffee";
+import { getCoffeeInfoByBlockProperties } from "./util";
 
 export const getCoffeeInfoList = async () => {
   const coffeeInfoList: CoffeeInfo[] = [];
@@ -10,26 +10,8 @@ export const getCoffeeInfoList = async () => {
     database_id: process.env.NOTION_DATABASE_ID ?? "",
   })) {
     if ("properties" in block) {
-      const coffeeInfo = Object.entries(block.properties).map(
-        ([key, value]) => {
-          let fieldValue = DEFAULT_FIELD_VALUE;
-          if (value.type === "title") {
-            fieldValue = value.title[0]?.plain_text;
-          } else if (value.type === "rich_text") {
-            fieldValue = value.rich_text[0]?.plain_text;
-          } else if (value.type === "unique_id") {
-            const { prefix, number } = value.unique_id;
-            fieldValue = `${prefix}-${number}`;
-          } else if (value.type === "number") {
-            fieldValue = value.number;
-          } else {
-            fieldValue = JSON.stringify(value);
-          }
-          return { [key]: fieldValue ?? DEFAULT_FIELD_VALUE };
-        }
-      );
-
-      coffeeInfoList.push(Object.assign({}, ...coffeeInfo));
+      const coffeeInfo = getCoffeeInfoByBlockProperties(block);
+      coffeeInfoList.push(coffeeInfo);
     }
   }
 
@@ -53,22 +35,7 @@ export const getCoffeeInfoById = async (id: number) => {
 
   const coffeeInfo: CoffeeInfo[] = data.results.map((block) => {
     if (!("properties" in block)) return;
-    const coffeeInfo = Object.entries(block.properties).map(([key, value]) => {
-      let fieldValue = DEFAULT_FIELD_VALUE;
-      if (value.type === "title") {
-        fieldValue = value.title[0]?.plain_text;
-      } else if (value.type === "rich_text") {
-        fieldValue = value.rich_text[0]?.plain_text;
-      } else if (value.type === "unique_id") {
-        const { prefix, number } = value.unique_id;
-        fieldValue = `${prefix}-${number}`;
-      } else {
-        fieldValue = JSON.stringify(value);
-      }
-      return { [key]: fieldValue ?? DEFAULT_FIELD_VALUE };
-    });
-
-    return Object.assign({}, ...coffeeInfo);
+    return getCoffeeInfoByBlockProperties(block);
   });
 
   return coffeeInfo[0];
