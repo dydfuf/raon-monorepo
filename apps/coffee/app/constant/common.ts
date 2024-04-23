acd5f2350bb33a59e6063682565624210ea9a3f9
dydfuf <88dydfuf@naver.com>
2024-04-24 01:30:58 +0900

    ✨ feat(app): [raon-60] 불필요한 파일을 제거합니다
    Changelog: feature

--- apps/coffee/app/constant/common.ts
+++ apps/coffee/app/constant/common.ts
@@ -22,6 +22,3 @@ export const siteConfig = {
     },
   ],
 };
-
-export const IS_CLIENT = typeof window !== "undefined";
-
--- apps/coffee/app/utils/common.ts
+++
@@ -1,7 +0,0 @@
-import { IS_CLIENT } from "../constant/common";
-
-export const scrollToTop = () => {
-  if (IS_CLIENT) {
-    document.documentElement.scrollTo({ top: 0, behavior: "smooth" });
-  }
-};
