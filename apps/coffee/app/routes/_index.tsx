96ba8620c67052fab07cf86436c94e6fabe6ed85
dydfuf <88dydfuf@naver.com>
2024-05-05 03:04:50 +0900

    ✨ feat(app): [raon-62] 404페이지 구현 및 클라이언트 에러를 수정합니다
    Changelog: feature

--- apps/coffee/app/routes/_index.tsx
+++ apps/coffee/app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import type { MetaFunction, HeadersFunction } from "@vercel/remix";
 import CommandMenu from "../components/command-menu";
 import { siteConfig } from "../constant/common";
+import { Link } from "@remix-run/react";
+import { Button } from "@raonc/ui/components/button";
 
 export const headers: HeadersFunction = ({}) => ({
   "Cache-Control": "max-age=300, s-maxage=3600",
@@ -32,6 +34,29 @@ export default function Index() {
         <p className="text-[1rem] sm:text-[1.25rem]">
           당신의 취향에 맞는 완벽한 커피 원두를 찾아보세요!
         </p>
+        <h3 className="text-xl font-semibold pt-16">내 취향을 모르겠다면?</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-4 md:w-[400px]">
+          <Link to={"/coffee/list?nation=에티오피아"} className="w-full">
+            <Button className="w-full font-semibold" variant={"secondary"}>
+              에티오피아 원두 보러가기
+            </Button>
+          </Link>
+          <Link to={"/coffee/list?nation=과테말라"} className="w-full">
+            <Button className="w-full font-semibold" variant={"secondary"}>
+              과테말라 원두 보러가기
+            </Button>
+          </Link>
+          <Link to={"/coffee/list?note=포도"} className="w-full">
+            <Button className="w-full font-semibold" variant={"secondary"}>
+              포도 맛 원두 보러가기
+            </Button>
+          </Link>
+          <Link to={"/coffee/list?note=복숭아"} className="w-full">
+            <Button className="w-full font-semibold" variant={"secondary"}>
+              복숭아 맛 원두 보러가기
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
