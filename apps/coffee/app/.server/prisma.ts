ee21bde504168907e697ec98bec96cae80fd02eb
dydfuf <88dydfuf@naver.com>
2024-06-02 01:58:00 +0900

    ✨ feat(app): [raon-83] turso를 적용합니다
    Changelog: feature

--- apps/coffee/app/.server/prisma.ts
+++ apps/coffee/app/.server/prisma.ts
@@ -1,9 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+
 declare global {
   var __prisma: PrismaClient;
 }
 if (!global.__prisma) {
-  global.__prisma = new PrismaClient();
+  const libsql = createClient({
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  });
+
+  const adapter = new PrismaLibSQL(libsql);
+  global.__prisma = new PrismaClient({ adapter });
 }
 global.__prisma.$connect();
 export const prisma = global.__prisma;
