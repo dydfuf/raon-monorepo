405931c96019acbd3affdbe80d7af960976d32d7
Raon <41016331+dydfuf@users.noreply.github.com>
2024-06-30 00:36:31 +0900

    ✨ feat(app): [raon-92] 홈화면의 content card ui를 마이그레이션 합니다 (#70)
    * ✨ feat(app): [raon-92] 홈화면의 content card ui를 마이그레이션 합니다
    
    Changelog: feature
    
    * 🐛 fix(app): [raon-92] max-width에 px를 누락한 부분 수정합니다
    
    Changelog: fix
    
    * 🐛 fix(app): [raon-92] description 섹션을 center정렬 되도록 수정합니다
    
    Changelog: fix
    
    * ✨ feat(app): [raon-92] 타이틀 height를 고정으로 가져가도록 합니다
    
    Changelog: feature

---
+++ apps/blog/components/common/ContentCard.tsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@raonc/ui/components/card";
+import { Badge } from "@raonc/ui/components/badge";
+
+interface Props {
+  title: string;
+  timeToRead: string;
+  description: string;
+  releaseDate: string;
+  categories: string[];
+}
+
+export default function ContentCard({
+  title,
+  timeToRead,
+  description,
+  releaseDate,
+  categories,
+}: Props) {
+  return (
+    <Card className="h-full">
+      <CardHeader className="pb-3">
+        <CardTitle className="text-2xl h-16 line-clamp-2">{title}</CardTitle>
+        <CardDescription className="text-center">
+          {timeToRead} · {releaseDate}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex gap-x-4 gap-y-2 flex-wrap">
+          {categories.map((category) => (
+            <Badge key={category} className="rounded-full">
+              {category}
+            </Badge>
+          ))}
+        </div>
+        <p className="text-sm text-muted-foreground mt-4">{description}</p>
+      </CardContent>
+    </Card>
+  );
+}
