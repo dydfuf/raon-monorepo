7fe5b6514ab2d68b57326b710ce143cc0e200f78
dydfuf <88dydfuf@naver.com>
2024-06-30 00:17:31 +0900

    ✨ feat(app): [raon-92] 홈화면의 content card ui를 마이그레이션 합니다
    Changelog: feature

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,3 +1,39 @@
+import Link from "next/link";
+import ContentCard from "../components/common/ContentCard";
+import { getPostByName, getPostNameList } from "../utils/post";
+import { parseMarkdownMetadata } from "../utils/parseMarkdownMetadata";
+import readingTime from "reading-time";
+
 export default function MainPage() {
-  return <div>page</div>;
+  const postList = getPostNameList().map((postName) => ({
+    name: postName,
+    content: getPostByName(postName ?? ""),
+  }));
+
+  return (
+    <div className="w-full">
+      <div className="w-full gap-5 grid grid-cols-1 p-5 md:grid-cols-2 max-w-[1024] mx-auto">
+        {postList.map((post) => {
+          const { name, content } = post;
+          const { date, description, category, title } =
+            parseMarkdownMetadata(content);
+          const stats = readingTime(content);
+
+          return (
+            <article key={name}>
+              <Link href={`/posts/${name}`}>
+                <ContentCard
+                  title={title ?? ""}
+                  timeToRead={stats.text}
+                  description={description ?? ""}
+                  releaseDate={date ?? ""}
+                  categories={category?.split(",") ?? []}
+                />
+              </Link>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
---
+++ apps/blog/components/common/ContentCard.tsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@raonc/ui/components/card";
+import { Badge } from "@raonc/ui/components/badge";
+
+interface Props {
+  title: string;
+  timeToRead: string;
+  description: string;
+  releaseDate: string;
+  categories: string[];
+}
+
+export default function ContentCard({
+  title,
+  timeToRead,
+  description,
+  releaseDate,
+  categories,
+}: Props) {
+  return (
+    <Card className="h-full">
+      <CardHeader className="pb-3">
+        <CardTitle className="text-2xl">{title}</CardTitle>
+        <CardDescription>
+          {timeToRead} · {releaseDate}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="">
+        <div className="flex flex-wrap gap-x-4 gap-y-2">
+          {categories.map((category) => (
+            <Badge key={category} className="rounded-full">
+              {category}
+            </Badge>
+          ))}
+        </div>
+        <p className="text-sm text-muted-foreground mt-4">{description}</p>
+      </CardContent>
+    </Card>
+  );
+}
