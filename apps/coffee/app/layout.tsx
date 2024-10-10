e1b8f11576dc548f74044dc15c9c7e64082972c2
dydfuf <88dydfuf@naver.com>
2024-10-10 23:05:08 +0900

    feat: 홈화면 메타데이터를 추가합니다.

--- apps/coffee/app/layout.tsx
+++ apps/coffee/app/layout.tsx
@@ -1,7 +1,109 @@
+import "@raonc/ui/globals.css";
 import { TailwindIndicator } from "../components/TailwindIndicator";
 import { ThemeProvider } from "../components/ThemeProvider";
 import SiteHeader from "../components/SiteHeader";
-import "@raonc/ui/globals.css";
+import { Metadata } from "next";
+import { siteConfig } from "../constants/siteConfig";
+
+export const metadata: Metadata = {
+  title: siteConfig.name,
+  description: siteConfig.description,
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      url: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    { rel: "manifest", url: "/manifest.json" },
+    { rel: "apple-touch-icon", sizes: "57x57", url: "/apple-icon-57x57.png" },
+    { rel: "apple-touch-icon", sizes: "60x60", url: "/apple-icon-60x60.png" },
+    { rel: "apple-touch-icon", sizes: "72x72", url: "/apple-icon-72x72.png" },
+    { rel: "apple-touch-icon", sizes: "76x76", url: "/apple-icon-76x76.png" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "114x114",
+      url: "/apple-icon-114x114.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "120x120",
+      url: "/apple-icon-120x120.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "144x144",
+      url: "/apple-icon-144x144.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "152x152",
+      url: "/apple-icon-152x152.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/apple-icon-180x180.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/android-icon-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      url: "/favicon-96x96.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    { rel: "manifest", url: "/manifest.json" },
+  ],
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    url: `https://${siteConfig.domain}`,
+    type: "website",
+    images: [
+      {
+        url: `https://${siteConfig.domain}/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    images: [`https://${siteConfig.domain}/og-image.png`],
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+  },
+};
 
 export default function RootLayout({
   children,
